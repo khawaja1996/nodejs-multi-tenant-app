8cb9bfbf1c37fc1d0259adf4afaf1535bfaee0ec
khawaja1996 <hamzarazzaq96@gmail.com>
2024-03-29 16:31:00 +0500

    initial commit

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 echo 'here';
 ?>
+PORT=3000
+DB_URL=mongodb+srv://tenants:nKbkhQr4grmjpzPC@cluster0.hi2lkdm.mongodb.net/
+SECRET_KEY=mysecretkey
+
+HOSTS file
+127.0.0.1       tenant1.tenant.local
+127.0.0.1       tenant2.tenant.local
